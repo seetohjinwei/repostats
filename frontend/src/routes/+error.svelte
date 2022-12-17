e3f5c751ff4e332867ad012b36ebef950789cf25
Jin Wei <seetohjinwei@gmail.com>
2022-12-17 20:50:59 +0800

    Add +error.svelte

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<h1 class="title">RepoStats</h1>
+<p>Page doesn't seem to exist! Back to <a href="/">home</a>?</p>
+
+<style lang="scss">
+	@import "../styles/colours.scss";
+
+	.title {
+		font-size: 4em;
+		margin: 0;
+	}
+
+	p {
+		font-size: 1.2em;
+	}
+</style>
